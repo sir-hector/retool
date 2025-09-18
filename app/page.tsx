5a6aedb5388cb94a3ea5f3a2c848000dca2d6901
Karol <krauskarol7@gmail.com>
2025-09-18 19:39:59 +0200

    add button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <Button />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
