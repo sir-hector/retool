28f3fa9375ce6b7b93e5dd13dd03a9b08a8c68b0
Karol <krauskarol7@gmail.com>
2025-10-09 18:18:17 +0200

    example button component

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <Button />
+        <Button title="Click">Click me </Button>
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
