f1e63434f1e1229e96b2776949b752569be20326
Karol <krauskarol7@gmail.com>
2025-10-09 18:40:19 +0200

    navbar update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,18 @@
+import { Moon } from "lucide-react";
+import Link from "next/link";
+
 const Navbar = () => {
-  return <div className="">Navbar</div>;
+  return (
+    <nav className="p-4 flex items-center justify-between">
+      {/* LEFT */}
+      collapsebutton
+      {/* RIGHT */}
+      <div className="flex items-center">
+        <Link href="/">Dashboard</Link>
+        <Moon />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
