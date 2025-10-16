e8b935bf00751d4b8529729ce207e098609645fc
Karol <krauskarol7@gmail.com>
2025-10-16 18:18:07 +0200

    Sidebar trigger

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <div className="">
-      <Button />
-    </div>
-  );
+  return <div className="">{/* <Button /> */}</div>;
 }
