6dd51a39cfdb93d121dbd70f2bb410223cc67be0
Karol <krauskarol7@gmail.com>
2025-10-18 20:42:13 +0200

    base chart component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import AppBarChart from "@/components/AppBarChart";
+
 export default function Home() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2">
-        TEST
+        <AppBarChart />
       </div>
       <div className="bg-primary-foreground p-4 rounded-lg">TEST</div>
       <div className="bg-primary-foreground p-4 rounded-lg">TEST</div>
