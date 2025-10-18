cf1cc9d1be6b76a9bd924010a9ce4965d5600d83
Karol <krauskarol7@gmail.com>
2025-10-18 20:28:08 +0200

    Home page - test components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
-  return <div className="">{/* <Button /> */}</div>;
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2">
+        TEST
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">TEST</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">TEST</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">TEST</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">TEST</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">TEST</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">TEST</div>
+    </div>
+  );
 }
