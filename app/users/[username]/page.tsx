8e032672d91a6bbad9ee3002f687dc0604ea4742
Karol <krauskarol7@gmail.com>
2025-10-21 20:56:00 +0200

    user page structure

--- app/users/[username]/page.tsx
+++ app/users/[username]/page.tsx
@@ -1,3 +1,4 @@
+import CardList from "@/components/CardList";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -30,6 +31,27 @@ const SingleUserPage = () => {
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
+      {/* CONTAINER */}
+      <div className="mt-4 flex flex-col gap-8 xl:flex-row">
+        {/* LEFT */}
+        <div className="w-full xl:w-1/3 space-y-6">
+          {/* USER BADGES CONTAINER */}
+          <div className="bg-primary-foreground p-4 rounded-lg">Badge</div>
+          {/* INFORMATION CONTAINER */}
+          <div className="bg-primary-foreground p-4 rounded-lg">Info</div>
+          {/* CARD LIST */}
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            <CardList title="Recent Transactions" />
+          </div>
+        </div>
+        {/* RIGHT */}
+        <div className="w-full xl:w-2/3 space-y-6">
+          {/* USER CARD CONTAINER */}
+          <div className="bg-primary-foreground p-4 rounded-lg">User Card</div>
+          {/* CHART CONTAINER */}
+          <div className="bg-primary-foreground p-4 rounded-lg">Chart</div>
+        </div>
+      </div>
     </div>
   );
 };
